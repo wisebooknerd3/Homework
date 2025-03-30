170667224c57509c15676189995de8c244c2364c
wisebooknerd3 <147564579+wisebooknerd3@users.noreply.github.com>
2025-03-29 21:15:57 -0700

    secondCommit

---
+++ name.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin>>n;
+
+    int arr[n][n];
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>arr[i][j];
+        }
+    }
+
+
+    for(int i=0; i<n; i++){
+
+        if(i%2==0){
+            for(int j=0; j<n; j++){
+                cout<<arr[i][j]<<" ";
+            }
+        }
+        else{
+            for(int j=n-1; j>=0; j--){
+                cout<<arr[i][j]<<" ";
+            }
+        }
+    }
+
+}
+
+// 10 20 30 40 ---> 45
